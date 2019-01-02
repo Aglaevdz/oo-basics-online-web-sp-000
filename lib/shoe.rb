1cce343680e568f6cf089c03db5922066f05dc8c
Aglae Verduzco <aglae.cv@gmail.com>
2019-01-02 02:22:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :brand
-  attr_reader :title
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+  def cobble
+    puts "Your shoe is as good as new!"
   end
 
 end
